52c471e5149cf6bec5b54ed39da96b6f79326e88
Yana Nazarova <yankaya15@gmail.com>
2019-03-26 23:47:35 +0300

    Modified Table

--- CatalogBlock.js
+++ CatalogBlock.js
@@ -9,11 +9,11 @@ var CatalogBlock = React.createClass({
     render: function() {
   
       var productsCode=[];
-      //this.props.products.forEach(function(item){
+      //products.prototype.forEach(function(item){
         for ( var a=0; a<this.props.products.length; a++ ) {
-            var product=this.props.products[a];
+            var product=this.props.products[item];
             var productCode=        
-            React.DOM.tr({key:product.code,className:'Product'},
+            React.DOM.tr ({key:product.code,className:'Product'},
                 React.DOM.td({className:'Text'},product.text),
                 React.DOM.td({className:'Price'},product.price),
                 React.DOM.td({className:'Img'},product.img),
@@ -22,9 +22,9 @@ var CatalogBlock = React.createClass({
             productsCode.push(productCode);
       };
       
-      return React.DOM.div( {className:'CatalogBlock'}, 
-        React.DOM.div( {className:'Market'}, this.props.market ),
-        React.DOM.table( {className:'Products'}, productsCode ),
+      return React.DOM.table( {className:'CatalogBlock'}, 
+        React.DOM.thead( {className:'Market'}, this.props.market ),
+        React.DOM.tbody( {className:'Products'}, productsCode ),
       );
     },
   
