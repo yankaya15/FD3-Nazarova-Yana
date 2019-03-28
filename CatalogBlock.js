11454409daded5b286e48fd0e6fdee1fe86082ca
Yana Nazarova <yankaya15@gmail.com>
2019-03-28 19:14:47 +0300

    corrected

--- CatalogBlock.js
+++ CatalogBlock.js
@@ -5,13 +5,15 @@ var CatalogBlock = React.createClass({
     getDefaultProps: function() {
       return { market: "Магазин скоро откроется" }
     },
-  
+    
+    propTypes: {
+      products: React.PropTypes.array.isRequired,
+    },
+
     render: function() {
   
       var productsCode=[];
-      //products.prototype.forEach(function(item){
-        for ( var a=0; a<this.props.products.length; a++ ) {
-            var product=this.props.products[item];
+      this.props.products.forEach(function(product){
             var productCode=        
             React.DOM.tr ({key:product.code,className:'Product'},
                 React.DOM.td({className:'Text'},product.text),
@@ -20,10 +22,11 @@ var CatalogBlock = React.createClass({
                 React.DOM.td({className:'Count'},product.count),
             );
             productsCode.push(productCode);
-      };
+      }
+        );
       
       return React.DOM.table( {className:'CatalogBlock'}, 
-        React.DOM.thead( {className:'Market'}, this.props.market ),
+        React.DOM.caption( {className:'Market'}, this.props.market ),
         React.DOM.tbody( {className:'Products'}, productsCode ),
       );
     },
